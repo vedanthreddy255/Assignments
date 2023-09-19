ae472db20983d6f0c3df803ee74d9d0f4d7acadc
vedanthreddy255 <72791314+vedanthreddy255@users.noreply.github.com>
2023-09-19 17:01:02 +0530

    Add files via upload

---
+++ Day2/assignment2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Demo1
+{
+    public delegate void Mydelegate();
+
+    internal class Program
+    {
+
+     
+        static void Main(string[] args)
+        { 
+            
+            TestClass obj=new TestClass();
+            Mydelegate delobj = new Mydelegate(obj.Hello);
+            delobj();
+            Console.WriteLine();
+        }
+
+    }
+    internal class TestClass()
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+
+}
